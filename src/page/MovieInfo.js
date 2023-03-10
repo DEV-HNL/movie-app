950611784e3a7b7e8a59d1b85cd9ab32a9c2d85f
DEV-HNL <tlchnl05@gmail.com>
2023-03-10 15:24:42 +0700

    3

--- src/page/MovieInfo.js
+++ src/page/MovieInfo.js
@@ -2,15 +2,65 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { fetcher, keyApi } from "../config";
 import useSWR from "swr";
+import "./style.scss";
 const MovieInfo = () => {
   const { slug } = useParams();
-  const { data, error, isloading } = useSWR(
+  const { data } = useSWR(
     `https://api.themoviedb.org/3/movie/${slug}?api_key=${keyApi}`,
     fetcher
   );
-  console.log(data);
-  console.log(slug);
-  return <div></div>;
+  const {
+    title,
+    backdrop_path,
+    overview,
+    release_date,
+    vote_average,
+    vote_count,
+    popularity,
+  } = data || {};
+  return (
+    <>
+      <div className="movieinfo">
+        <div className="movieinfo__bg">
+          <img
+            src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+            alt=""
+          />
+        </div>
+        <div className="movieinfo__content">
+          <div className="movieinfo__poster">
+            <img
+              src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+              alt=""
+            />
+            <div className="movieinfo__poster--rated">
+              <span className="rated">{vote_average}</span>
+              <span>{vote_count} ratings</span>
+            </div>
+          </div>
+          <div className="movieinfo__info">
+            <div className="movieinfo__info--header">
+              <h2>{title}</h2>
+              <div className="movieinfo__info--date">
+                <span>{release_date}</span>
+                <span>{popularity} view</span>
+              </div>
+              <div className="movieinfo__info--watch">
+                <button className="watch-now">Watch now!</button>
+                <button className="share">
+                  <i className="fa-solid fa-share"></i>
+                </button>
+                <button className="bookmark">
+                  <i className="fa-solid fa-bookmark"></i>
+                </button>
+              </div>
+            </div>
+            <p className="movies__desc">{overview}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieInfo;
